71aa37aa2e39315cf7e62051cfb877850b68709f
Nickchen Nick <nickchen120235@gmail.com>
2020-10-27 09:09:14 +0800

    add titlebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { CssBaseline } from '@material-ui/core'
 
-function App() {
+import { TitleBar } from './component/TitleBar'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseline />
+      <TitleBar title='PRP Assignment 3' />
+      <p>Hello React!</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/TitleBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+
+import styles from '../style/TitleBar'
+
+interface TitleBarProps {
+  title: string
+}
+
+export const TitleBar = (props: TitleBarProps) => {
+  const classes = styles()
+  return (
+    <>
+      <AppBar className={classes.appbar}>
+        <Toolbar>
+          <Typography variant='h4'>{props.title}</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  )
+} 
