526ef21eb909aa528798d58ae2a117cfe6dead65
Nickchen Nick <nickchen120235@gmail.com>
2020-10-29 00:55:11 +0800

    add marginLeft to selectors

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,9 @@ const App = () => {
       <Paper className={classes.graph}>
         <Typography variant='h5' align='center'>Percentage of Customers</Typography>
         <FormControl>
-          <FormLabel component='legend'>VIEW MODE</FormLabel>
+          <FormLabel component='legend' style={{marginLeft: '5px'}}>VIEW MODE</FormLabel>
           <RadioGroup row value={selected} onChange={(e, value) => {setSelected(value)}}>
-            {selectorOptions.map((value, index) => <FormControlLabel key={index} value={value} control={<Radio />} label={value} />)}
+            {selectorOptions.map((value, index) => <FormControlLabel key={index} value={value} control={<Radio />} label={value} style={{marginLeft: '5px'}} />)}
           </RadioGroup>
         </FormControl>
         <Graph mode={selected} />
