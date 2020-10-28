7813dfa5d5f8fdc05f3dd4bd7d0527ddb6ff0577
Nickchen Nick <nickchen120235@gmail.com>
2020-10-29 00:55:52 +0800

    add label for axes

--- src/component/Graph.tsx
+++ src/component/Graph.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlexibleWidthXYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries } from 'react-vis'
+import { FlexibleWidthXYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, ChartLabel } from 'react-vis'
 
 import { data } from './data'
 
@@ -8,63 +8,65 @@ interface GraphProps {
 }
 type RenderDataType = {
   x: number,
-  y: number
+  y: number,
+  color: string
 }
 
 const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+const colors = ['#00838f', '#039be5', '#1a237e', '#ffc107', '#e65100', '#7b1fa2']
 
 export const Graph = (props: GraphProps) => {
-  let renderData: RenderDataType[][] = []
-
   // It is ugly but it should work.
+  let renderData: RenderDataType[][] = []
   switch(props.mode){
   case 'gender':
     renderData = [
-      data.map((value, index) => ({x: index+1, y: value.female})),
-      data.map((value, index) => ({x: index+1, y: 100-value.female}))
+      data.map((value, index) => ({x: index+1, y: value.female, color: colors[0]})),
+      data.map((value, index) => ({x: index+1, y: 100-value.female, color: colors[1]}))
     ]
     break
   case 'location':
     renderData = [
-      data.map((value, index) => ({x: index+1, y: value.local})),
-      data.map((value, index) => ({x: index+1, y: value.USA})),
-      data.map((value, index) => ({x: index+1, y: value.SA})),
-      data.map((value, index) => ({x: index+1, y: value.EU})),
-      data.map((value, index) => ({x: index+1, y: value.MEA})),
-      data.map((value, index) => ({x: index+1, y: value.ASIA})),
+      data.map((value, index) => ({x: index+1, y: value.local, color: colors[0]})),
+      data.map((value, index) => ({x: index+1, y: value.USA, color: colors[1]})),
+      data.map((value, index) => ({x: index+1, y: value.SA, color: colors[2]})),
+      data.map((value, index) => ({x: index+1, y: value.EU, color: colors[3]})),
+      data.map((value, index) => ({x: index+1, y: value.MEA, color: colors[4]})),
+      data.map((value, index) => ({x: index+1, y: value.ASIA, color: colors[5]})),
     ]
     break
   case 'type':
     renderData = [
-      data.map((value, index) => ({x: index+1, y: value.businessmen})),
-      data.map((value, index) => ({x: index+1, y: value.tourists}))
+      data.map((value, index) => ({x: index+1, y: value.businessmen, color: colors[0]})),
+      data.map((value, index) => ({x: index+1, y: value.tourists, color: colors[1]}))
     ]
     break
   case 'reserve':
     renderData = [
-      data.map((value, index) => ({x: index+1, y: value.DR})),
-      data.map((value, index) => ({x: index+1, y: value.agency})),
-      data.map((value, index) => ({x: index+1, y: value.AC})),
+      data.map((value, index) => ({x: index+1, y: value.DR, color: colors[0]})),
+      data.map((value, index) => ({x: index+1, y: value.agency, color: colors[1]})),
+      data.map((value, index) => ({x: index+1, y: value.AC, color: colors[2]})),
     ]
     break
   case 'age':
     renderData = [
-      data.map((value, index) => ({x: index+1, y: value.u20})),
-      data.map((value, index) => ({x: index+1, y: value.to35})),
-      data.map((value, index) => ({x: index+1, y: value.to55})),
-      data.map((value, index) => ({x: index+1, y: value.m55})),
+      data.map((value, index) => ({x: index+1, y: value.u20, color: colors[0]})),
+      data.map((value, index) => ({x: index+1, y: value.to35, color: colors[1]})),
+      data.map((value, index) => ({x: index+1, y: value.to55, color: colors[2]})),
+      data.map((value, index) => ({x: index+1, y: value.m55, color: colors[3]})),
     ]
     break
   default:
     break
   }
+
   return(
-    <>
-      <FlexibleWidthXYPlot height={500} style={{margin: '5px'}} stackBy='y' animation>
-        <VerticalGridLines /><HorizontalGridLines />
-        <XAxis tickFormat={v => months[v-1]} /><YAxis />
-        {renderData.map((value, index) => <VerticalBarSeries key={index} data={value} barWidth={.5} />)}
-      </FlexibleWidthXYPlot>
-    </>
+    <FlexibleWidthXYPlot height={550} stackBy='y' animation style={{marginLeft: '5px', marginBottom: '5px'}}>
+      <VerticalGridLines /><HorizontalGridLines />
+      <XAxis tickFormat={v => months[v-1]} top={500} /><YAxis left={25} />
+      <ChartLabel id='x' text='Month' xPercent={.5}   yPercent={.92} />
+      <ChartLabel id='y' text='%'     xPercent={.008} yPercent={.4}  />
+      {renderData.map((value, index) => <VerticalBarSeries key={index} data={value} barWidth={.5} colorType='literal' />)}
+    </FlexibleWidthXYPlot>
   )
 }
