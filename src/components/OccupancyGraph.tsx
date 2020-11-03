58efa66b531c52ca52aa01c946f246aac18a3413
Nickchen Nick <nickchen120235@gmail.com>
2020-11-03 17:12:16 +0800

    add OccupancyGraph

---
+++ src/components/OccupancyGraph.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Legend } from 'recharts'
+
+import { data } from '../utils/data'
+import { months } from '../utils/consts'
+import { OccupancyDataType } from '../utils/types'
+
+export const OccupancyGraph = () => {
+  const renderData: OccupancyDataType[] = data.map((value, index) => ({
+    month: months[index],
+    occ: value.occupancy
+  }))
+
+  return(
+    <ResponsiveContainer width='95%' height={300}>
+      <LineChart data={renderData} margin={{top: 20, right: 20, left: 20, bottom: 20}}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='month' />
+        <YAxis domain={[50, 100]} tickCount={6} tickFormatter={value => `${value}%`} />
+        <Legend />
+        <Line name='Occupancy' dataKey='occ' dot={{r: 5}} animationEasing='ease-in-out' />
+      </LineChart>
+    </ResponsiveContainer>
+  )
+}
